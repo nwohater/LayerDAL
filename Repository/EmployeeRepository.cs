d7853750ff8ef5794c8c7686333b901950417c77
Brandon Lackey <bslackey@gmail.com>
2022-11-16 15:28:39 -0800

    Add project files.

---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LayerDAL.Setting;
+using LayerDAL.Entities;
+using Microsoft.Extensions.Options;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace LayerDAL.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ConnectionSetting _connection;
+
+        public EmployeeRepository(IOptions<ConnectionSetting> connection)
+        {
+            _connection = connection.Value;
+
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            List<Employee> ListEmployees = new List<Employee>();
+
+            using (var connect = new SqlConnection(_connection.SQLString))
+            {
+                connect.Open();
+                SqlCommand cmd = new SqlCommand("sp_getEmployees", connect);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        ListEmployees.Add(new Employee()
+                        {
+                            IdEmployee = Convert.ToInt32(reader["IdEmployee"]),
+                            Name = reader["Name"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            Address = reader["Address"].ToString(),
+                            Phone = reader["Phone"].ToString()
+                            
+                        });
+                    }
+                }
+            }
+            return ListEmployees;
+        }
+    }
+}
---
+++ Repository/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LayerDAL.Entities;
+
+namespace LayerDAL.Repository   
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetEmployees();
+    }
+}
